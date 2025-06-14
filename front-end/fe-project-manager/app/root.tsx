5ca81b559894be0bfcf52721bd86039c3183b7cc
MT1607 <nguyengiason5@gmail.com>
2025-06-15 00:14:27 +0700

    update meta function

--- front-end/fe-project-manager/app/root.tsx
+++ front-end/fe-project-manager/app/root.tsx
@@ -23,6 +23,14 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "GiaSon-PrM" },
+    { name: "description", content: "Welcome to Project Manager of Gia Son" },
+  ];
+}
+
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
