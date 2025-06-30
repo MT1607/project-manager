05b24f89f979167f0ed96c1ccfa8cc7eb6d00408
MT1607 <nguyengiason5@gmail.com>
2025-06-30 23:23:29 +0700

    check-logout

--- front-end/fe-project-manager/app/routes/dashboard/dashboard-layout.tsx
+++ front-end/fe-project-manager/app/routes/dashboard/dashboard-layout.tsx
@@ -1,6 +1,13 @@
+import {useAuth} from "~/provider/auth-context";
+import {Button} from "~/components/ui/button";
+
 const DashboardLayout = () => {
+    const {logout} = useAuth();
+
     return (
-        <>DashboardLayout</>
+        <>
+            <Button onClick={logout}>Logout</Button>
+        </>
     )
 }
 
