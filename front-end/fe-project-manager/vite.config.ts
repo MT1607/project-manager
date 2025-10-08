117792a8a0bd82b4cb491e36ffd89e41d588324e
MT1607 <nguyengiason5@gmail.com>
2025-10-08 19:35:06 +0700

    docker: update docker compose file

--- front-end/fe-project-manager/vite.config.ts
+++ front-end/fe-project-manager/vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/prm/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
