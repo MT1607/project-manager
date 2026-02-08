c3fc2eb67fb30a3bce822a5cf6f2f425f0cb9f4a
MT1607 <nguyengiason5@gmail.com>
2026-02-08 22:26:05 +0700

    chore: check status be is working

--- front-end/fe-project-manager/app/lib/fetch-utils.ts
+++ front-end/fe-project-manager/app/lib/fetch-utils.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api-v1`,
   headers: {
     'Content-Type': 'application/json',
   },
